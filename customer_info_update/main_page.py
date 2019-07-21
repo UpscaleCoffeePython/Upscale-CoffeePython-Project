b9e7d23dcf3e2a8f0422c9aa786a90b143db3ff9
paristan <51906792+paristan@users.noreply.github.com>
2019-07-21 17:26:18 +0800

    Update main_page.py
    included nygel's changes, cleaned nygel's changes, cleaned customer info

--- customer_info_update/main_page.py
+++ customer_info_update/main_page.py
@@ -22,7 +22,7 @@ def index():
 
     return html + menu_title + menu + link
 
-#-----------------------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------
     # Attempt to process 2nd Page
 @app.route('/orders', methods = ['GET','POST'])
 def orders():
@@ -36,43 +36,127 @@ def orders():
     
     menu2 = '<table><tr><th>Code</th><th>Name</th><th>Price</th></tr>{}</table></div>'.format(menu_string2)
 
+    #form = '''
+        #<form method="POST">
+        #Enter Code: <input type="text" name="code"/><br/>
+        #Enter Quantity: <input type="number" name="qty"/><br/>
+        #<input type="submit"/>
+        #</form>
+        #'''
+    instruc2 = "<h4>Please choose a quantity first then click the product.</h4>"
     form = '''
         <form method="POST">
-        Enter Code: <input type="text" name="code"/><br/>
-        Enter Quantity: <input type="number" name="qty"/><br/>
-        <input type="submit"/>
+        Enter Quantity: <input type="number" name="qtyAmr"/><br/>
+        <input type="submit" name="americano" value="americano"/><br/>
+        
+        Enter Quantity: <input type="number" name="qtyBre"/><br/>
+        <input type="submit" name="brewed" value="brewed"/><br/>
+        
+        Enter Quantity: <input type="number" name="qtyCap"/><br/>
+        <input type="submit" name="cappuccino" value="cappuccino"/><br/>
+        
+        Enter Quantity: <input type="number" name="qtyEsp"/><br/>
+        <input type="submit" name="espresso" value="espresso"/><br/>
+        
+        Enter Quantity: <input type="number" name="qtyLat"/><br/>
+        <input type="submit" name="latte" value="latte"/><br/>
+        
+        Enter Quantity: <input type="number" name="qtyFra"/><br/>
+        <input type="submit" name="frappuccino" value="frappuccino"/><br/>
+        
+        Enter Quantity: <input type="number" name="qtyMoc"/><br/>
+        <input type="submit" name="mocha" value="mocha"/><br/>
+        
+        Enter Quantity: <input type="number" name="qtyMac"/><br/>
+        <input type="submit" name="macchiato" value="macchiato"/><br/>
+        
+        Enter Quantity: <input type="number" name="qtyAff"/><br/>
+        <input type="submit" name="affogato" value="affogato"/><br/>
+        
+        Enter Quantity: <input type="number" name="qtyCor"/><br/>
+        <input type="submit" name="cortado" value="cortado"/><br/>
+        
+        Enter Quantity: <input type="number" name="qtyBla"/><br/>
+        <input type="submit" name="black" value="black"/><br/>
+        
+        Enter Quantity: <input type="number" name="qtyIce"/><br/>
+        <input type="submit" name="iced" value="iced"/><br/>
         </form>
         '''
+
     food_tray = []
     if 'food_tray' not in session:
         session['food_tray'] = []
     else:
         food_tray = session['food_tray']
-        code=request.form.get("code")
-        qty=request.form.get("qty")
+        
+        if request.form.get('americano') == 'americano':
+            code = "americano"
+            qty=request.form.get("qtyAmr")
+        elif request.form.get('brewed') == 'brewed':
+            code = "brewed"
+            qty=request.form.get("qtyBre")
+        elif request.form.get('cappuccino') == 'cappuccino':
+            code = "cappuccino"
+            qty=request.form.get("qtyCap")
+        elif request.form.get('espresso') == 'espresso':
+            code = "espresso"
+            qty=request.form.get("qtyEsp")
+        elif request.form.get('latte') == 'latte':
+            code = "latte"
+            qty=request.form.get("qtyLat")
+        elif request.form.get('frappuccino') == 'frappuccino':
+            code = "frappuccino"
+            qty=request.form.get("qtyFra")
+        elif request.form.get('mocha') == 'mocha':
+            code = "mocha"
+            qty=request.form.get("qtyMoc")
+        elif request.form.get('macchiato') == 'macchiato':
+            code = "macchiato"
+            qty=request.form.get("qtyMac")
+        elif request.form.get('affogato') == 'affogato':
+            code = "affogato"
+            qty=request.form.get("qtyAff")
+        elif request.form.get('cortado') == 'cortado':
+            code = "cortado"
+            qty=request.form.get("qtyCor")
+        elif request.form.get('black') == 'black':
+            code = "black"
+            qty=request.form.get("qtyBla")
+        elif request.form.get('iced') == 'iced':
+            code = "iced"
+            qty=request.form.get("qtyIce")
+
         prc=int(qty)*coffee_dict.products_dict[code]["price"]
-        food_tray.append({"code":code,"qty":qty,"price":prc})
+        food_tray.append({"code":code,"quantity":qty,"price":prc})
         session['food_tray']=food_tray
+
+        #food_tray = session['food_tray']
+        #code=request.form.get("code")
+        #qty=request.form.get("qty")
+        #prc=int(qty)*coffee_dict.products_dict[code]["price"]
+        #food_tray.append({"code":code,"qty":qty,"price":prc})
+        #session['food_tray']=food_tray
     # print(session)
     print(food_tray)
     # print(models.products[request.form.get("code")]["name"])
-    food_tray_string = ["<div>{}-{}-{}</div>".format(i["code"],i["qty"],i["price"]) for i in food_tray]
+    food_tray_string = ["<div>{}-{}-{}</div>".format(i["code"],i["quantity"],i["price"]) for i in food_tray]
     
     link2 = '<div> {} | </div>'.format('<a href="cinfo">Proceed to Customer Information</a>')
     
-    return html + instruc + menu_title2 + menu2 + form + "".join(food_tray_string) + link2
+    return html + instruc2 + menu_title2 + menu2 + form + "".join(food_tray_string) + link2
 
 if __name__ == '__main__':
     app.config['SESSION_TYPE'] = 'filesystem'
     app.run(debug=True, port = 5678)
 
 
-#-------------------------------------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------
     # Attempt to process 3rd Page
 @app.route('/cinfo', methods = ['GET','POST'])
 def cinfo():
     html = "<html><h1>Customer Information</h1></html>"
-    instruc2 = "<h4>Kindly input the following information</h4>"
+    instruc3 = "<h4>Kindly input the following information</h4>"
     required = "<h5>All fields are required</h5>"
     message = "<h4>Thank You for submitting your information!</h4>"
 
@@ -131,7 +215,7 @@ def cinfo():
     
     message_after_string = ["<div>{}</div>".format(i["message"]) for i in message_after]
     
-    return html + instruc2 + required + form + "".join(customer_info_string) + "".join(message_after_string)
+    return html + instruc3 + required + form + "".join(customer_info_string) + "".join(message_after_string)
 
 if __name__ == '__main__':
     app.config['SESSION_TYPE'] = 'filesystem'
