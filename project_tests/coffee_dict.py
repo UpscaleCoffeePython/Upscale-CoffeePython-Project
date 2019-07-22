80e66099b10259264f14032e701dc968305f1bc3
Gene Nygel Ramos <nygelramos@gmail.com>
2019-07-22 15:32:16 +0800

    fixed orders page

---
+++ project_tests/coffee_dict.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+products_dict = {'americano': {'name': 'Americano', 'price': 150.0},
+                 'brewed': {'name': 'Brewed Coffee', 'price': 120.0},
+                 'cappuccino': {'name': 'Cappuccino', 'price': 170.0},
+                 'espresso': {'name': 'Espresso', 'price': 140.0},
+                 'latte': {'name': 'Latte', 'price': 110.0},
+                 'frappuccino': {'name': 'Frappuccino', 'price': 190.0},
+                 'mocha': {'name' : 'Caffe Mocha', 'price': 160.0},
+                 'macchiato': {'name': 'Caffe Macchiato', 'price': 170.0},
+                 'affogato': {'name': 'Affogato', 'price': 200.0},
+                 'cortado': {'name': 'Cortado', 'price': 130.0},
+                 'black': {'name': 'Black Coffee', 'price': 120.0},
+                 'iced': {'name': 'Iced Coffee', 'price': 130.0}} 
+
